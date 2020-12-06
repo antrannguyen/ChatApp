c1711398ccda8f400ccfc510690e0e81a9ac7afa
An Tran <thuyan1149@gmail.com>
2020-12-07 01:34:31 +0200

    added some middlewares

--- src/server/index.js
+++ src/server/index.js
@@ -1,32 +1,32 @@
 import express from "express";
-// import path from "path";
-// import helmet from "helmet";
-// import cors from "cors";
-// import compress from "compression";
+import path from "path";
+import helmet from "helmet";
+import cors from "cors";
+import compress from "compression";
 // import services from "./services";
 
-// const root = path.join(__dirname, "../../");
+const root = path.join(__dirname, "../../");
 const app = express();
 
 // if (process.env.NODE_ENV === "production") {
-// 	app.use(helmet());
-// 	app.use(
-// 		helmet.contentSecurityPolicy({
-// 			directives: {
-// 				defaultSrc: ["'self'"],
-// 				scriptSrc: ["'self'", "'unsafe-inline'"],
-// 				styleSrc: ["'self'", "'unsafe-inline'"],
-// 				imgSrc: ["'self'", "data:", "*.amazonaws.com"],
-// 			},
-// 		})
-// 	);
+app.use(helmet());
+app.use(
+	helmet.contentSecurityPolicy({
+		directives: {
+			defaultSrc: ["'self'"],
+			scriptSrc: ["'self'", "'unsafe-inline'"],
+			styleSrc: ["'self'", "'unsafe-inline'"],
+			imgSrc: ["'self'", "data:", "*.amazonaws.com"],
+		},
+	})
+);
 // 	app.use(compress());
 // 	app.use(cors());
 // }
 
-// app.use(helmet.referrerPolicy({ policy: "same-origin" }));
-// app.use("/", express.static(path.join(root, "dist/client")));
-// app.use("/uploads", express.static(path.join(root, "uploads")));
+app.use(helmet.referrerPolicy({ policy: "same-origin" }));
+app.use("/", express.static(path.join(root, "dist/client")));
+app.use("/uploads", express.static(path.join(root, "uploads")));
 
 // const serviceNames = Object.keys(services);
 // for (let i = 0; i < serviceNames.length; i += 1) {
@@ -38,11 +38,7 @@ const app = express();
 // 	}
 // }
 
-app.get("*", (req, res) => {
-	res.send("HelloWorld");
+app.get("/", (req, res) => {
+	res.sendFile(path.join(root, "/dist/client/index.html"));
 });
-// app.get("/", (req, res) => {
-// eslint-disable-next-line no-tabs
-// 	res.sendFile(path.join(root, "/dist/client/index.html"));
-// });
 app.listen(8000, () => console.log("Listening on port 8000!"));
