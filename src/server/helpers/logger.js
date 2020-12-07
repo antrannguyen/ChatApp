27290497b99723ecc16874e2a6fe703e1b6d6c54
An Tran <thuyan1149@gmail.com>
2020-12-07 16:32:29 +0200

    added logger from winston

--- src/server/helpers/logger.js
+++ src/server/helpers/logger.js
@@ -1,24 +1,24 @@
-import winston from 'winston';
+import winston from "winston";
 
-let transports = [
-  new winston.transports.File({
-    filename: 'error.log',
-    level: 'error',
-  }),
-  new winston.transports.File({
-    filename: 'combined.log',
-    level: 'verbose',
-  }),
+const transports = [
+	new winston.transports.File({
+		filename: "error.log",
+		level: "error",
+	}),
+	new winston.transports.File({
+		filename: "combined.log",
+		level: "verbose",
+	}),
 ];
 
-if (process.env.NODE_ENV !== 'production') {
- transports.push(new winston.transports.Console());
+if (process.env.NODE_ENV !== "production") {
+	transports.push(new winston.transports.Console());
 }
 
 const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  transports,
+	level: "info",
+	format: winston.format.json(),
+	transports,
 });
 
 export default logger;
