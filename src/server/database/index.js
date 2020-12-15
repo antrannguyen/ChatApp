c2fa573fe29883864d698f49bf318896ffb0062e
An Tran <thuyan1149@gmail.com>
2020-12-15 17:37:18 +0200

    added some middlewares

--- src/server/database/index.js
+++ src/server/database/index.js
@@ -1,15 +1,29 @@
-import Sequelize from 'sequelize';
-import configFile from '../config/';
-import models from '../models';
+import Sequelize from "sequelize";
+// import configFile from '../config/';
+// import models from '../models';
 
-const env = process.env.NODE_ENV || 'development';
-const config = configFile[env];
+// const env = process.env.NODE_ENV || 'development';
+// const config = configFile[env];
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+// const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-const db = {
-    models: models(sequelize),
-    sequelize,
-};
+// const db = {
+//     models: models(sequelize),
+//     sequelize,
+// };
 
-export default db;
+// export default db;
+
+const sequelize = new Sequelize("chatappDB", "root", "root", {
+	host: "localhost",
+	dialect: "mysql",
+	operatorsAliases: false,
+	pool: {
+		max: 5,
+		min: 0,
+		acquire: 30000,
+		idle: 10000,
+	},
+});
+
+export default sequelize;
